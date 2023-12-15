e30e078405c969925dd55921da17026fb7e412f3
alissondemidio <alissond1995@gmail.com>
2023-12-15 13:40:37 -0300

    Update flatgrid_test.dart

--- supergrid/test/flatgrid_test.dart
+++ supergrid/test/flatgrid_test.dart
@@ -1,9 +1,72 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:supergrid/supergrid.dart';
+import 'package:supergrid/flatgrid.dart';
 
 void main() {
-  test('adds one to input values', () {
+  group('FlatGridView', () {
+    testWidgets('renders empty grid with "No items" text',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: FlatGridView(
+              data: const [],
+              renderItem: (data) => Container(),
+              itemWidth: 100,
+              itemHeight: 100,
+            ),
+          ),
+        ),
+      );
 
+      expect(find.text('No items'), findsOneWidget);
+    });
+
+    testWidgets('renders grid with items', (WidgetTester tester) async {
+      final List<String> testData = ['Item 1', 'Item 2', 'Item 3'];
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: FlatGridView(
+              data: testData,
+              renderItem: (data) => Text(data),
+              itemWidth: 100,
+              itemHeight: 100,
+            ),
+          ),
+        ),
+      );
+
+      expect(find.text('Item 1'), findsOneWidget);
+      expect(find.text('Item 2'), findsOneWidget);
+      expect(find.text('Item 3'), findsOneWidget);
+    });
+
+    testWidgets('calls onPressed callback when item is pressed',
+        (WidgetTester tester) async {
+      int pressedIndex = -1;
+
+      final List<String> testData = ['Item 1', 'Item 2', 'Item 3'];
+
+      await tester.pumpWidget(
+        MaterialApp(
+          home: Scaffold(
+            body: FlatGridView(
+              data: testData,
+              renderItem: (data) => Text(data),
+              itemWidth: 100,
+              itemHeight: 100,
+              onPressed: (index) {
+                pressedIndex = index;
+              },
+            ),
+          ),
+        ),
+      );
+
+      await tester.tap(find.text('Item 2'));
+      expect(pressedIndex, 1);
+    });
   });
 }
