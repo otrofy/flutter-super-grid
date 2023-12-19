67756b7e4adbd858b9e5314e9aaa0ab3e2c2de7e
alissondemidio <alissond1995@gmail.com>
2023-12-19 13:45:06 -0300

    Update sectiongrid_test.dart

--- test/sectiongrid_test.dart
+++ test/sectiongrid_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:supergrid/sectiongrid.dart';
+import 'package:flutter_super_grid/sectiongrid.dart';
 
 void main() {
   group('SectionGridView Tests', () {
