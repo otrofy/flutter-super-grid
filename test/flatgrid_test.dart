ca1a061f0e089d5fc794a87ee32ef34776d301ca
Elias Dayan <elias.dayan@otrofy.com>
2023-12-19 12:10:34 -0600

    changing imports

--- test/flatgrid_test.dart
+++ test/flatgrid_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_super_grid/flatgrid.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('FlatGridView Tests', () {
--- test/sectiongrid_test.dart
+++ test/sectiongrid_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_super_grid/sectiongrid.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('SectionGridView Tests', () {
