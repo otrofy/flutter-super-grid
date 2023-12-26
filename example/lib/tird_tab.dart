da15a6f861d0b2d880fd422a7671b82c71537d5c
eliday15 <eliasdayanc@gmail.com>
2023-12-26 17:20:25 -0600

    Corrected fixed simplegrid

--- example/lib/tird_tab.dart
+++ example/lib/tird_tab.dart
@@ -12,56 +12,56 @@ class TirdTab extends StatelessWidget {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
         child: SimpleGridView(
-      data: data,
-      renderItem: (itemData) {
-        return Container(
-          height: 10 * (data.indexOf(itemData) * 1 + 1),
-          width: 10 * (data.indexOf(itemData) * 1 + 1),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(
-                15.0), // Ajusta el radio según sea necesario
-            child: CachedNetworkImage(
-              imageUrl: itemData,
-              placeholder: (context, url) =>
-                  const CircularProgressIndicator(), // Puedes personalizar el indicador de carga
-              errorWidget: (context, url, error) => const Icon(Icons.error),
-              fit: BoxFit
-                  .cover, // Ajusta el modo de ajuste de la imagen según sea necesario
+            data: data,
+            renderItem: (itemData) {
+              return Container(
+                height: 10 * (data.indexOf(itemData) * 1 + 1),
+                width: 10 * (data.indexOf(itemData) * 1 + 1),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(
+                      15.0), // Ajusta el radio según sea necesario
+                  child: CachedNetworkImage(
+                    imageUrl: itemData,
+                    placeholder: (context, url) =>
+                        const CircularProgressIndicator(), // Puedes personalizar el indicador de carga
+                    errorWidget: (context, url, error) =>
+                        const Icon(Icons.error),
+                    fit: BoxFit
+                        .cover, // Ajusta el modo de ajuste de la imagen según sea necesario
+                  ),
+                ),
+              );
+            },
+            itemSize: 80.0,
+            itemsPerRow: 3,
+            isFixed: true,
+            style: const SimpleGridViewStyle(
+              color: Colors.green,
+              padding: EdgeInsets.all(16.0),
+              gridViewPadding: EdgeInsets.all(24.0),
             ),
-          ),
-        );
-      },
-      itemSize: 80.0,
-      itemsPerRow: 3,
-      isFixed: false,
-      style: const SimpleGridViewStyle(
-        color: Colors.green,
-        padding: EdgeInsets.all(16.0),
-        gridViewPadding: EdgeInsets.all(24.0),
-      ),
-      itemHeight: 100,
-      itemWidth: 100,
-      additionalRow: Container(
-        height: 100,
-        width: 420,
-        color: Colors.red,
-        child: const Row(
-          children: [
-            Icon(
-              Icons.abc,
-              color: Colors.white,
-            ),
-            Text(
-              "Foot Render Widget",
-              style: TextStyle(color: Colors.white),
-            )
-          ],
-        ),
-      ),
-      verticalSpacing: 10.0,
-      horizontalSpacing: 10.0,
-      horizontal: true,
-      invertedRow: true,
-    ));
+            itemHeight: 100,
+            itemWidth: 100,
+            verticalSpacing: 10.0,
+            horizontalSpacing: 10.0,
+            horizontal: true,
+            invertedRow: true,
+            additionalRow: Container(
+              height: 100,
+              width: 420,
+              color: Colors.red,
+              child: const Row(
+                children: [
+                  Icon(
+                    Icons.abc,
+                    color: Colors.white,
+                  ),
+                  Text(
+                    "Foot Render Widget",
+                    style: TextStyle(color: Colors.white),
+                  )
+                ],
+              ),
+            )));
   }
 }
